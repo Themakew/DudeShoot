9e4c212df9f0502b633ffe8d91b16538df5f7a75
Ruyther Costa <ruy_fusion@hotmail.com>
2016-06-02 19:21:30 -0300

    Adding the background and element on the screen

---
+++ load.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class LoadResources:
+    player = pygame.image.load("resources/images/dude.png")
+    grass = pygame.image.load("resources/images/grass.png")
+    castle = pygame.image.load("resources/images/castle.png")
--- main.py
+++ main.py
@@ -1,27 +1,49 @@
-# 1 - Import library
 import pygame
-from pygame.locals import *
+from load import LoadResources
 
-# 2 - Initialize the game
-pygame.init()
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
 
-# 3 - Load images
-player = pygame.image.load("resources/images/dude.png")
+class Game:
+    load = LoadResources()
 
-# 4 - keep looping through
-while 1:
-    # 5 - clear the screen before drawing it again
-    screen.fill(0)
-    # 6 - draw the screen elements
-    screen.blit(player, (200, 100))
-    # 7 - update the screen
-    pygame.display.flip()
-    # 8 - loop through the events
-    for event in pygame.event.get():
-        # check if the event is the X button
-        if event.type == pygame.QUIT:
-            # if it is quit the game
-            pygame.quit()
-            exit(0)
+    pygame.init()
+    width, height = 640, 480
+    screen = pygame.display.set_mode((width, height))
+    keys = [False, False, False, False]
+    player_position = [100, 100]
+
+    def draw_background(self):
+        for x in range(self.width / self.load.grass.get_width() + 1):
+            for y in range(self.height / self.load.grass.get_height() + 1):
+                self.screen.blit(self.load.grass, (x * 100, y * 100))
+
+    def draw_elements_on_the_screen(self):
+        self.screen.blit(self.load.player, (100, 100))
+        self.screen.blit(self.load.castle, (0, 30))
+        self.screen.blit(self.load.castle, (0, 135))
+        self.screen.blit(self.load.castle, (0, 240))
+        self.screen.blit(self.load.castle, (0, 345))
+
+    def game_loop(self):
+        while 1:
+            # 5 - clear the screen before drawing it again
+            self.screen.fill(0)
+
+            # 6 - draw the screen elements
+            self.draw_background()
+            self.draw_elements_on_the_screen()
+
+            # 7 - update the screen
+            pygame.display.flip()
+
+            # 8 - loop through the events
+            for event in pygame.event.get():
+                # check if the event is the X button
+                if event.type == pygame.QUIT:
+                    # if it is quit the game
+                    pygame.quit()
+                    exit(0)
+
+
+if __name__ == "__main__":
+    main = Game()
+    main.game_loop()
