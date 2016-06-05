fc547c68a9b224544121ff86f2a371fe839a399f
Ruyther Costa <ruy_fusion@hotmail.com>
2016-06-04 23:13:42 -0300

    Adding the win and lose check

--- load.py
+++ load.py
@@ -3,8 +3,12 @@ import pygame
 
 class LoadResources:
     player = pygame.image.load("resources/images/dude.png")
-    grass = pygame.image.load("resources/images/grass.png")
+    grass = pygame.image.load("resources/images/grass.jpg")
     castle = pygame.image.load("resources/images/castle.png")
     arrow = pygame.image.load("resources/images/bullet.png")
     bad_guy_image_one = pygame.image.load("resources/images/badguy.png")
     bad_guy_image = bad_guy_image_one
+    health_bar = pygame.image.load("resources/images/healthbar.png")
+    health = pygame.image.load("resources/images/health.png")
+    gameover = pygame.image.load("resources/images/gameover.png")
+    you_win = pygame.image.load("resources/images/youwin.png")
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import math
 import random
 
 
+# 90 seconds
+GAMEPLAY_RUNNING_TIME = 90000
+
+
 class Game:
     load = LoadResources()
 
@@ -20,13 +24,14 @@ class Game:
     bad_timer_one = 0
     enemies = [[640, 100]]
     health_value = 194
+    running = 1
+    exit_code = 1
+    accuracy = 0
 
     bad_rect = pygame.Rect(load.bad_guy_image.get_rect())
 
     def draw_background(self):
-        for x in range(self.width / self.load.grass.get_width() + 1):
-            for y in range(self.height / self.load.grass.get_height() + 1):
-                self.screen.blit(self.load.grass, (x * 100, y * 100))
+        self.screen.blit(self.load.grass, (0, 0))
 
     def draw_elements_on_the_screen(self):
         self.screen.blit(self.load.castle, (0, 30))
@@ -73,6 +78,22 @@ class Game:
         for enemie in self.enemies:
             self.screen.blit(self.load.bad_guy_image, enemie)
 
+    def draw_clock(self):
+        font = pygame.font.Font(None, 24)
+        survived_text = font.render(
+            str((GAMEPLAY_RUNNING_TIME - pygame.time.get_ticks()) / 60000) + ":" +
+            str((GAMEPLAY_RUNNING_TIME - pygame.time.get_ticks()) / 1000 % 60).zfill(2),
+            True,
+            (0, 0, 0))
+        text_rect = survived_text.get_rect()
+        text_rect.topright = [635, 5]
+        self.screen.blit(survived_text, text_rect)
+
+    def draw_health_bar(self):
+        self.screen.blit(self.load.health_bar, (5, 5))
+        for health_one in range(self.health_value):
+            self.screen.blit(self.load.health, (health_one + 8, 8))
+
     def move_player(self):
         if self.keys[0]:
             self.player_position[1] -= 5
@@ -112,7 +133,7 @@ class Game:
             self.enemies.pop(index)
 
     def check_collision(self, index):
-        index1 = 0
+        index_one = 0
         for bullet in self.arrows:
             bull_rect = pygame.Rect(self.load.arrow.get_rect())
             bull_rect.left = bullet[1]
@@ -120,11 +141,52 @@ class Game:
             if self.bad_rect.colliderect(bull_rect):
                 self.shoot_accuracy[0] += 1
                 self.enemies.pop(index)
-                self.arrows.pop(index1)
-            index1 += 1
+                self.arrows.pop(index_one)
+            index_one += 1
+
+    def check_if_player_win_or_lose(self):
+        if pygame.time.get_ticks() >= 90000:
+            self.running = 0
+            self.exit_code = 1
+        if self.health_value <= 0:
+            self.running = 0
+            self.exit_code = 0
+        if self.shoot_accuracy[1] != 0:
+            self.accuracy = self.shoot_accuracy[0] * 1.0 / self.shoot_accuracy[1] * 100
+        else:
+            self.accuracy = 0
+
+    def check_if_player_exit(self, event):
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+
+    def display_win_or_lose(self):
+        if self.exit_code == 0:
+            pygame.font.init()
+            font = pygame.font.Font(None, 24)
+            text = font.render("Accuracy: " + str(self.accuracy) + "%", True, (255, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.centerx = self.screen.get_rect().centerx
+            text_rect.centery = self.screen.get_rect().centery + 24
+            self.screen.blit(self.load.gameover, (0, 0))
+            self.screen.blit(text, text_rect)
+        else:
+            pygame.font.init()
+            font = pygame.font.Font(None, 24)
+            text = font.render("Accuracy: " + str(self.accuracy) + "%", True, (0, 255, 0))
+            text_rect = text.get_rect()
+            text_rect.centerx = self.screen.get_rect().centerx
+            text_rect.centery = self.screen.get_rect().centery + 24
+            self.screen.blit(self.load.you_win, (0, 0))
+            self.screen.blit(text, text_rect)
+        while 1:
+            for event in pygame.event.get():
+                self.check_if_player_exit(event)
+            pygame.display.flip()
 
     def game_loop(self):
-        while 1:
+        while self.running:
             self.bad_timer -= 1
             # 5 - clear the screen before drawing it again
             self.screen.fill(0)
@@ -134,6 +196,8 @@ class Game:
             self.draw_elements_on_the_screen()
             self.draw_arrows()
             self.draw_enemies()
+            self.draw_clock()
+            self.draw_health_bar()
             self.player_movement(self.angle_between_mouse_and_Player())
 
             # 7 - update the screen
@@ -141,11 +205,7 @@ class Game:
 
             # 8 - loop through the events
             for event in pygame.event.get():
-                # check if the event is the X button
-                if event.type == pygame.QUIT:
-                    # if it is quit the game
-                    pygame.quit()
-                    exit(0)
+                self.check_if_player_exit(event)
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_w:
@@ -170,6 +230,8 @@ class Game:
                 self.arrow_movement(event.type)
 
             self.move_player()
+            self.check_if_player_win_or_lose()
+        self.display_win_or_lose()
 
 
 if __name__ == "__main__":
